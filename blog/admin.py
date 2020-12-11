7f061a388176bdb604021fe0be51a2aa3c48183f
William E. Seyler <wseyler@pentaho.com>
2020-12-10 19:07:32 -0500

    Changed how blogs and comments are handled.

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Post, Comment
 
+class CommentInline(admin.StackedInline):
+    model = Comment
+    extra = 1
+
 class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
     list_display = ('title', 'slug', 'status','created_on')
     list_filter = ("status",)
     search_fields = ['title', 'content']
@@ -9,12 +14,12 @@ class PostAdmin(admin.ModelAdmin):
 
 admin.site.register(Post, PostAdmin)
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'body', 'post', 'created_on', 'active')
-    list_filter = ('active', 'created_on')
-    search_fields = ('name', 'email', 'body')
-    actions = ['approve_comments']
-
-    def approve_comments(self, request, queryset):
-        queryset.update(active=True)
+# @admin.register(Comment)
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'body', 'post', 'created_on', 'active')
+#     list_filter = ('active', 'created_on')
+#     search_fields = ('name', 'author', 'body')
+#     actions = ['approve_comments']
+#
+#     def approve_comments(self, request, queryset):
+#         queryset.update(active=True)
--- blog/models.py
+++ blog/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
     name = models.CharField(max_length=80)
-    email = models.EmailField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=False)
