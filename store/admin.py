5d4fca53b7551f21af87b260f34552b1fb58c3ed
William E. Seyler <wseyler@pentaho.com>
2021-01-17 20:47:34 -0500

    Allow filtering on if there is a date_completed for an invoice

--- store/admin.py
+++ store/admin.py
@@ -5,15 +5,12 @@ class LineItemInline(admin.TabularInline):
     model = LineItem
     extra = 0
 
-
-
 @admin.register(Invoice)
 class InvoiceAdmin(admin.ModelAdmin):
     model = Invoice
     inlines = [LineItemInline]
-    list_display = ['id', 'purchaser', 'date_ordered', 'date_completed', 'invoice_total', 'is_completed']
+    list_display = ['id', 'purchaser', 'date_ordered', 'date_completed', 'invoice_total']
     list_display_links = ['id', 'purchaser', 'date_ordered', 'date_completed', 'invoice_total']
-    def is_completed(self, obj):
-        return obj.date_completed != None
+    list_filter = [('date_completed', admin.EmptyFieldListFilter)]
 
 admin.site.register(Item)
