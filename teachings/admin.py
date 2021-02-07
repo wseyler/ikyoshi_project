e53a1168992e085deabd4e227646d84a6f28c30f
William E. Seyler <wseyler@pentaho.com>
2021-02-07 14:48:02 -0500

    Fixed default admin class start time ordering

--- teachings/admin.py
+++ teachings/admin.py
@@ -6,6 +6,7 @@ class TrainingClassAdmin(admin.ModelAdmin):
     list_display = ['start', 'end', 'duration_in_mins', 'notes']
     list_display_links = ['start', 'end', 'duration_in_mins']
     list_filter=['start']
+    ordering = ('-start',)
     fieldsets = (
         ('Class Info', {
             'fields' : ('start', 'end', 'focus', 'notes')
